cd20840c4c0cc960d2c7aba1d1e28fd553ffb79d
Artem Malyshev <proofit404@gmail.com>
2020-01-27 02:04:41 +0300

    refactor: rewrite Success marker to be without keyword arguments

--- src/_stories/returned.py
+++ src/_stories/returned.py
@@ -6,18 +6,6 @@ class Result(object):
         return "Result(" + repr(self.value) + ")"
 
 
-class Success(object):
-    def __init__(self, **kwargs):
-        self.kwargs = kwargs
-
-    def __repr__(self):
-        return (
-            "Success("
-            + ", ".join([k + "=" + repr(v) for k, v in self.kwargs.items()])
-            + ")"
-        )
-
-
 class Failure(object):
     def __init__(self, reason=None):
         self.reason = reason
@@ -25,6 +13,11 @@ class Failure(object):
     def __repr__(self):
         reason = repr(self.reason) if self.reason else ""
         return "Failure(" + reason + ")"
+
+
+class Success(object):
+    def __repr__(self):
+        return "Success()"
 
 
 class Skip(object):
